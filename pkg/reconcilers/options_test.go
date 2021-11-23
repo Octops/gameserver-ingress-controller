a870956fdc278d8e558f176bc1c4d74d5578ac6f
Daniel Oliveira <daniel.oliveira079@gmail.com>
2021-11-23 19:46:09 +0100

    Support custom secret name on WithTLS option

--- pkg/reconcilers/options_test.go
+++ pkg/reconcilers/options_test.go
@@ -100,7 +100,7 @@ func Test_WithCustomAnnotations(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			gs := newGameServer(tc.annotations)
+			gs := newGameServer("simple-gameserver", "default", tc.annotations)
 
 			ingress, err := newIngress(gs, WithCustomAnnotations())
 			if tc.wantErr {
@@ -119,6 +119,96 @@ func Test_WithCustomAnnotations(t *testing.T) {
 			for k, _ := range tc.notExpected {
 				require.NotContains(t, ingress.Annotations, k, "annotations %s should not present", k)
 			}
+		})
+	}
+}
+
+func Test_WithTLS(t *testing.T) {
+	testCases := []struct {
+		name        string
+		gsName      string
+		annotations map[string]string
+		routingMode gameserver.IngressRoutingMode
+		expected    string
+		wantErr     bool
+	}{
+		{
+			name:   "with custom secret name for domain mode",
+			gsName: "simple-gameserver-with-custom",
+			annotations: map[string]string{
+				gameserver.OctopsAnnotationIngressDomain:  "example.com",
+				gameserver.OctopsAnnotationsTLSSecretName: "my_custom_secret_name",
+			},
+			routingMode: gameserver.IngressRoutingModeDomain,
+			expected:    "my_custom_secret_name",
+		},
+		{
+			name:   "with custom secret name for path mode",
+			gsName: "simple-gameserver-with-custom",
+			annotations: map[string]string{
+				gameserver.OctopsAnnotationIngressFQDN:    "www.example.com",
+				gameserver.OctopsAnnotationsTLSSecretName: "my_custom_secret_name",
+			},
+			routingMode: gameserver.IngressRoutingModePath,
+			expected:    "my_custom_secret_name",
+		},
+		{
+			name:   "no custom secret name for domain mode",
+			gsName: "simple-gameserver-no-custom",
+			annotations: map[string]string{
+				gameserver.OctopsAnnotationIngressDomain: "example.com",
+			},
+			routingMode: gameserver.IngressRoutingModeDomain,
+			expected:    "simple-gameserver-no-custom-tls",
+		},
+		{
+			name:   "no custom secret name for path mode",
+			gsName: "simple-gameserver-no-custom",
+			annotations: map[string]string{
+				gameserver.OctopsAnnotationIngressFQDN: "www.example.com",
+			},
+			routingMode: gameserver.IngressRoutingModePath,
+			expected:    "simple-gameserver-no-custom-tls",
+		},
+		{
+			name:   "empty secret annotation for domain mode",
+			gsName: "simple-gameserver-no-custom",
+			annotations: map[string]string{
+				gameserver.OctopsAnnotationIngressDomain:  "example.com",
+				gameserver.OctopsAnnotationsTLSSecretName: "",
+			},
+			routingMode: gameserver.IngressRoutingModeDomain,
+			expected:    "",
+			wantErr:     true,
+		},
+		{
+			name:   "empty secret annotation for path mode",
+			gsName: "simple-gameserver-no-custom",
+			annotations: map[string]string{
+				gameserver.OctopsAnnotationIngressFQDN:    "www.example.com",
+				gameserver.OctopsAnnotationsTLSSecretName: "",
+			},
+			routingMode: gameserver.IngressRoutingModePath,
+			expected:    "",
+			wantErr:     true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			gs := newGameServer(tc.gsName, "default", tc.annotations)
+
+			ingress, err := newIngress(gs, WithTLS(tc.routingMode))
+			if tc.wantErr {
+				require.Error(t, err)
+				require.Nil(t, ingress)
+				require.Equal(t, "gameserver default/"+gs.Name+" has annotation "+gameserver.OctopsAnnotationsTLSSecretName+" but it is empty", err.Error())
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, ingress)
+				require.Equal(t, tc.expected, ingress.Spec.TLS[0].SecretName)
+			}
+
 		})
 	}
 }
