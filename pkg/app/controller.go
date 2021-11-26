f7fd1585daf9298e71a764b7cf028c923743d42c
Daniel Oliveira <daniel.oliveira079@gmail.com>
2021-11-26 18:17:34 +0100

    Add required configuration and flags to support additional endpoints
    - Health checks
    - Metrics

--- pkg/app/controller.go
+++ pkg/app/controller.go
@@ -3,19 +3,24 @@ package app
 import (
 	agonesv1 "agones.dev/agones/pkg/apis/agones/v1"
 	"context"
+	"fmt"
 	"github.com/Octops/gameserver-ingress-controller/internal/runtime"
 	"github.com/Octops/gameserver-ingress-controller/pkg/controller"
 	"github.com/Octops/gameserver-ingress-controller/pkg/handlers"
 	"github.com/Octops/gameserver-ingress-controller/pkg/k8sutil"
 	"github.com/Octops/gameserver-ingress-controller/pkg/manager"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 	"time"
 )
 
 type Config struct {
-	Kubeconfig string
-	SyncPeriod string
-	Port       int
-	Verbose    bool
+	Kubeconfig             string
+	SyncPeriod             string
+	Port                   int
+	Verbose                bool
+	HealthProbeBindAddress string
+	MetricsBindAddress     string
 }
 
 func StartController(ctx context.Context, config Config) error {
@@ -23,27 +28,39 @@ func StartController(ctx context.Context, config Config) error {
 
 	clientConf, err := k8sutil.NewClusterConfig(config.Kubeconfig)
 	if err != nil {
-		logger.Fatal(err)
+		withFatal(logger, err, "failed to create cluster config")
 	}
 
 	duration, err := time.ParseDuration(config.SyncPeriod)
 	if err != nil {
-		logger.WithError(err).Fatalf("error parsing sync-period flag: %s", config.SyncPeriod)
+		withFatal(logger, err, fmt.Sprintf("error parsing sync-period flag: %s", config.SyncPeriod))
 	}
 
 	mgr, err := manager.NewManager(clientConf, manager.Options{
-		SyncPeriod: &duration,
-		Port:       config.Port,
+		SyncPeriod:             &duration,
+		Port:                   config.Port,
+		HealthProbeBindAddress: config.HealthProbeBindAddress,
+		MetricsBindAddress:     config.MetricsBindAddress,
 	})
+	if err != nil {
+		withFatal(logger, err, "failed to create controller manager")
+	}
 
 	logger.Info("starting gameserver controller")
 	ctrl, err := controller.NewGameServerController(mgr, handlers.NewGameSeverEventHandler(clientConf, mgr.GetEventRecorderFor("gameserver-ingress-controller")), controller.Options{
 		For: &agonesv1.GameServer{},
 	})
+	if err != nil {
+		withFatal(logger, err, "failed to create controller")
+	}
 
 	if err := ctrl.Start(ctx); err != nil {
-		logger.Fatal(err)
+		withFatal(logger, err, "failed to start controller")
 	}
 
 	return nil
+}
+
+func withFatal(logger *logrus.Entry, err error, msg string) {
+	logger.Fatal(errors.Wrap(err, msg))
 }
