7456715454707fee248ba2355574d97bf4a26b58
Daniel Oliveira <daniel.oliveira079@gmail.com>
2023-08-05 12:33:51 +0200

    Expose maxconcurrent option

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -4,17 +4,21 @@ import (
 	"time"
 
 	"github.com/pkg/errors"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/config"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
 	"github.com/Octops/gameserver-ingress-controller/pkg/k8sutil"
 )
 
 type Options struct {
-	SyncPeriod             *time.Duration
-	Port                   int
-	HealthProbeBindAddress string
-	MetricsBindAddress     string
+	SyncPeriod              *time.Duration
+	Port                    int
+	HealthProbeBindAddress  string
+	MetricsBindAddress      string
+	MaxConcurrentReconciles int
 }
 
 type Manager struct {
@@ -22,14 +26,25 @@ type Manager struct {
 }
 
 func NewManager(kubeconfig string, options Options) (*Manager, error) {
-	config, err := k8sutil.NewClusterConfig(kubeconfig)
+	c, err := k8sutil.NewClusterConfig(kubeconfig)
 	if err != nil {
 		return nil, withError(errors.Wrap(err, "failed to create cluster config"))
 	}
 
-	mgr, err := manager.New(config, manager.Options{
-		SyncPeriod:             options.SyncPeriod,
-		Port:                   options.Port,
+	if options.MaxConcurrentReconciles <= 0 {
+		options.MaxConcurrentReconciles = 1
+	}
+
+	mgr, err := manager.New(c, manager.Options{
+		Cache: cache.Options{
+			SyncPeriod: options.SyncPeriod,
+		},
+		WebhookServer: webhook.NewServer(webhook.Options{
+			Port: options.Port,
+		}),
+		Controller: config.Controller{
+			MaxConcurrentReconciles: options.MaxConcurrentReconciles,
+		},
 		MetricsBindAddress:     options.MetricsBindAddress,
 		HealthProbeBindAddress: options.HealthProbeBindAddress,
 	})
